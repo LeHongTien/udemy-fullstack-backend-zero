452e564438389fbd999d780783650564a58f0f6d
LeHongTien <lehongtien157@gmail.com>
2025-03-05 21:39:55 +0700

    video 19

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+
 const app = express()
-const port = 8081
+const port = process.env.PORT || 8088;
+const hostname = process.env.HOST_NAME;
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -19,6 +22,6 @@ app.get('/hoidanit', (req, res) => {
     res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port,hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
