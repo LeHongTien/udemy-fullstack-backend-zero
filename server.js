3da18fce33e980817b27e305e2295906379809a6
LeHongTien <lehongtien157@gmail.com>
2025-03-05 19:19:10 +0700

    video 17

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express()
 const port = 8081
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.send('check ABC')
+})
+
+app.get('/hoidanit', (req, res) => {
+    res.send('<h1>hoi dan it voi eric</h1>')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
