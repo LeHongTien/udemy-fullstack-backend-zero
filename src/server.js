b53e0652486fdf33f86ff68600f7f07963f6cc83
LeHongTien <lehongtien157@gmail.com>
2025-03-06 09:25:48 +0700

    video 24

--- src/server.js
+++ src/server.js
@@ -1,28 +1,17 @@
-const express = require('express');
-const path = require('path');
 require('dotenv').config();
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
 
 const app = express()
 const port = process.env.PORT || 8088;
 const hostname = process.env.HOST_NAME;
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// config template engine
+configViewEngine(app)
 
-app.use(express.static(path.join(__dirname,'public')));
-
-app.get('/', (req, res) => {
-    res.send('Hello World! & nodemon update')
-})
-
-app.get('/abc', (req, res) => {
-    res.send('check ABC')
-})
-
-app.get('/hoidanit', (req, res) => {
-    // res.send('<h1>hoi dan it voi eric</h1>')
-    res.render('sample.ejs')
-})
+// khai báo Route
+app.use('/', webRoutes);
 
 app.listen(port,hostname, () => {
     console.log(`Example app listening on port ${port}`)
