48ba26e6663bbab25c0a4a2d1416a8441d122422
LeHongTien <lehongtien157@gmail.com>
2025-03-07 16:08:27 +0700

    video 31

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express()
 const port = process.env.PORT || 8088;
@@ -14,23 +14,12 @@ configViewEngine(app)
 // khai báo Route
 app.use('/', webRoutes);
 
-// test connection
-// Create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Tientolk050304@',
-    database: 'hoidanit',
-});
-
 // A simple SELECT query
 connection.query(
     'select * from users',
     function (err, results, fields) {
-      console.log(">> results= ", results); // results contains rows returned by server
-      console.log(">> fields= ",fields); // fields contains extra meta data about results, if available
-    }
+        console.log(">> results= ", results); // results contains rows returned by server
+}
 );
 app.listen(port,hostname, () => {
     console.log(`Example app listening on port ${port}`)
