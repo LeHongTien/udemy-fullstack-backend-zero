5dd08c32c96e1cc85e8caedd19e97baaba37e05b
LeHongTien <lehongtien157@gmail.com>
2025-03-07 21:23:37 +0700

    video 35

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,18 +1,7 @@
 const connection = require('../config/database');
 
 const getHomepage = (req, res) => {
-    let users = [];
-
-    connection.query(
-        'select * from users',
-        function (err, results, fields) {
-            users = results;
-            console.log(">> results= ", results); // results contains rows returned by server
-            // console.log(">>> check users: ", users);
-            res.send(JSON.stringify(users))
-        }
-    );
-    
+    return res.render('home.ejs')
 }
 
 const getABC = (req, res) => {
